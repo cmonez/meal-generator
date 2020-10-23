591030d1eb969be488469c57f4300ad953cc7eb9
Eric Zepeda <zepedaeric@gmail.com>
2020-10-22 21:40:02 -0700

    Use class component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import IngredientBox from './components/IngredientBox';
-const App = () => {
-  useEffect(() => {
-    // code to run on component mount
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foodItems: [],
+    };
+  }
+
+  componentDidMount() {
     const mealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
     Promise.all(new Array(10).fill(mealUrl).map((ea) => axios.get(ea))).then(
       (results) => {
-        console.log(results.map((meal) => meal.data.meals[0]));
+        const mealData = results.map((meal) => meal.data.meals[0]);
+        this.setState({
+          foodItems: mealData,
+        });
+        console.log(mealData);
       }
     );
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-      <h2>Hungry?</h2>
-      <IngredientBox />
-    </div>
-  );
-};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+        <h2>Hungry?</h2>
+        <IngredientBox />
+      </div>
+    );
+  }
+}
 
 export default App;
