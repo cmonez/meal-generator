4edb182bc14365d9dbc43515191b2bae15c6b6aa
Eric Zepeda <zepedaeric@gmail.com>
2020-10-22 21:46:34 -0700

    Refactor to hook component

--- src/App.js
+++ src/App.js
@@ -3,36 +3,29 @@ import axios from 'axios';
 import logo from './logo.svg';
 import IngredientBox from './components/IngredientBox';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foodItems: [],
-    };
-  }
+const App = () => {
+  const [meals, setMeals] = useState(
+    new Array(10).fill(0).map((ea) => 'mealURL')
+  );
 
-  componentDidMount() {
-    const mealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  const mealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+  useEffect(async () => {
     Promise.all(new Array(10).fill(mealUrl).map((ea) => axios.get(ea))).then(
       (results) => {
         const mealData = results.map((meal) => meal.data.meals[0]);
-        this.setState({
-          foodItems: mealData,
-        });
         console.log(mealData);
+        setMeals(mealData);
       }
     );
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header"></header>
-        <h2>Hungry?</h2>
-        <IngredientBox />
-      </div>
-    );
-  }
-}
+  }, [meals]);
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h2>Hungry?</h2>
+      <IngredientBox />
+    </div>
+  );
+};
 
 export default App;
