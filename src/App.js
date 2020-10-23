b6f11ac2c90a2a64b1d0671a7234c48ee2287538
Eric Zepeda <zepedaeric@gmail.com>
2020-10-22 21:35:39 -0700

    Create class based component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import IngredientBox from './components/IngredientBox';
-const App = () => {
-  useEffect(() => {
-    // code to run on component mount
-    axios
-      .get('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then(({ data }) => console.log(data.meals[0]));
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-      <h2>Hungry?</h2>
-      <IngredientBox />
-    </div>
-  );
-};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foodItems: [],
+    };
+  }
+
+  componentDidMount() {
+    const mealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    Promise.all(new Array(10).fill(mealUrl).map((ea) => axios.get(ea))).then(
+      (results) => {
+        const mealData = results.map((meal) => meal.data.meals[0]);
+        this.setState({
+          foodItems: mealData,
+        });
+        console.log(mealData);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+        <h2>Hungry?</h2>
+        <IngredientBox />
+      </div>
+    );
+  }
+}
 
 export default App;
