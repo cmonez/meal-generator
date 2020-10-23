0f5f24d527504f02950ede2989cdb9acb9458cec
Eric Zepeda <zepedaeric@gmail.com>
2020-10-22 21:38:32 -0700

    Create hook based call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import IngredientBox from './components/IngredientBox';
+
 const App = () => {
-  useEffect(() => {
-    // code to run on component mount
-    const mealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  const [meals, setMeals] = useState(
+    new Array(10).fill(0).map((ea) => 'mealURL')
+  );
+
+  const mealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+  useEffect(async () => {
     Promise.all(new Array(10).fill(mealUrl).map((ea) => axios.get(ea))).then(
       (results) => {
-        console.log(results.map((meal) => meal.data.meals[0]));
+        const mealData = results.map((meal) => meal.data.meals[0]);
+        console.log(mealData);
+        setMeals(mealData);
       }
     );
-  }, []);
+  }, [meals]);
   return (
     <div className="App">
       <header className="App-header"></header>
